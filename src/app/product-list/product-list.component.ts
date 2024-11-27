ee1d6102acd8f272a6e90c5c0646876f50027f8d
Javier Núñez <javi@javiernunez.com>
2024-11-27 06:34:40 +0100

    Add SASS styling

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,22 +7,32 @@ import { ProductService } from '../product.service';
   selector: 'app-product-list',
   standalone: true,
   imports: [FormsModule, NgForOf, NgIf],
+  styleUrl: './product-list.component.scss',
   template: `
-    <h3>Product List</h3>
-    <input
-      type="text"
-      [(ngModel)]="filterText"
-      (ngModelChange)="onFilterTextChange()"
-      placeholder="Search products"
-    />
-    <button (click)="addProduct()">Add Product</button>
-    <ul>
-      <li *ngFor="let product of displayedProducts">
-        {{ product.id }} | {{ product.name }} - {{ product.price }}
-      </li>
-    </ul>
-    <button *ngIf="hasMoreProducts" (click)="loadMore()">Show More</button>
-    <div *ngIf="isLoading">Loading...</div>
+    <div class="product-list">
+      <h3 class="title">Product List</h3>
+      <div class="controls">
+        <input
+            class="search-bar"
+            type="text"
+            [(ngModel)]="filterText"
+            (ngModelChange)="onFilterTextChange()"
+            placeholder="Search products..."
+        />
+        <button class="btn add-product-btn" (click)="addProduct()">Add Product</button>
+      </div>
+      <ul class="product-list-items">
+        <li *ngFor="let product of displayedProducts" class="product-item">
+          <div class="product-info">
+            <span class="product-id">#{{ product.id }}</span>
+            <span class="product-name">{{ product.name }}</span>
+          </div>
+          <span class="product-price">{{ product.price }}</span>
+        </li>
+      </ul>
+      <button *ngIf="hasMoreProducts" class="btn load-more-btn" (click)="loadMore()">Show More</button>
+      <div *ngIf="isLoading" class="loading-indicator">Loading...</div>
+    </div>
   `,
 })
 export class ProductListComponent implements OnInit {
